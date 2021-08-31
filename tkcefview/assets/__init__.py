0c7ff24ad4cc55914a2f249454420d270180cb70
eseunghwan <reignstarbits@gmail.com>
2021-09-01 07:44:42 +0900

    2021.09.01

--- tkcefview/assets/__init__.py
+++ tkcefview/assets/__init__.py
@@ -3,5 +3,5 @@ import os
 from . import __path__
 
 
-base_icon_file = os.path.join(__path__[0], "icon.png")
+base_icon_file = os.path.join(__path__[0], "ceftkview.png")
 vue_js_file = os.path.join(__path__[0], "vue.js")
